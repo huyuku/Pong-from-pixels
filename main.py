be8bdcc9b00d02737b64c67e1217f666a0ec0f2b
lagerros <jacobklagerros@gmail.com>
2018-02-03 16:28:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,22 +20,22 @@ agent = agents.BasicAgent(hidden_size, learning_rate)
 env = gym.make('Pong-v0')
 
 
-def main_function():
-	with tf.Session() as sess:
-		sess.run(tf.global_variables_initializer())
-		for i in range(num_iterations):
-			print("Iteration %s. Resetting dataset" % i)
-			train_data = game.train_set()
 
-			print("Starting self-play...")
-			for n in range(num_self_play_games):
-				if n%100 == 0:
-					print("Self-play game: %s" %n)
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	for i in range(num_iterations):
+		print("Iteration %s. Resetting dataset" % i)
+		train_data = game.train_set()
 
-			  self_play(sess, agent, env, train_data)
+		print("Starting self-play...")
+		for n in range(num_self_play_games):
+			if n%100 == 0:
+				print("Self-play game: %s" %n)
 
-			print("Starting training...")
-			for e in range(num_train_epochs):
-				diff_frames, actions, wins = train_data.sample(train_batch_size)
-				loss = agent.train(sess, diff_frames, actions, wins)
-				print("Loss epoch %s: %s" % (e, loss))
+		  self_play(sess, agent, env, train_data)
+
+		print("Starting training...")
+		for e in range(num_train_epochs):
+			diff_frames, actions, wins = train_data.sample(train_batch_size)
+			loss = agent.train(sess, diff_frames, actions, wins)
+			print("Loss epoch %s: %s" % (e, loss))
