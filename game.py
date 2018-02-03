85e066e6066af464946a7b2f1caed54d35d7ba81
unknown <joar.mvs@gmail.com>
2018-02-03 16:48:03 +0000

    added config

--- game.py
+++ game.py
@@ -4,6 +4,8 @@ This module is used to generate data by having the agent play pong.
 
 import agents
 import gym
+import cProfile
+import main
 
 #define preprocessing functions.
 def preprocess(frame):
@@ -23,7 +25,6 @@ def diff_frame(ordered_frames):
 	'''compares frames from the current and last timestep, in order to be able to capture motion'''
 	return preprocess(ordered_frames[0] - ordered_frames[1])
 
-<<<<<<< HEAD:testing.py
 #HYPERPARAMETERS AND SETTINGS
 num_self_play_games = 50000
 num_train_epochs = 100 #Change to something plausible later
@@ -33,8 +34,6 @@ learning_rate = 0.01
 hidden_size = 100
 without_net = False
 quick_self_play = True #For testing
-=======
->>>>>>> be8bdcc9b00d02737b64c67e1217f666a0ec0f2b:game.py
 
 debug = False
 displaying_analytics = False
@@ -98,7 +97,6 @@ def self_play(session, agent, env, train_data):
 
     env.render(close=True)
     return OpenAI_bot_score, agent_score
-<<<<<<< HEAD:testing.py
 
 #define preprocessing functions
 def preprocess(frame):
@@ -141,6 +139,7 @@ def main_function():
                 loss = agent.train(sess, diff_frames, actions, wins)
                 print("Loss epoch %s: %s" % (e, loss))
 
-cProfile.run('main_function()')
-=======
->>>>>>> be8bdcc9b00d02737b64c67e1217f666a0ec0f2b:game.py
+if main.print_analytics:
+	cProfile.run('main_function()')
+else:
+	main_function()
--- main.py
+++ main.py
@@ -5,37 +5,32 @@ import gym
 import cProfile
 import agents
 import game
-
-#HYPERPARAMETERS
-num_self_play_games = 50000
-num_train_epochs = 100 #Change to something plausible later
-train_batch_size = 20
-num_iterations = 1
-learning_rate = 0.01
-hidden_size = 100
-without_net = False
-quick_self_play = True #For testing
+from config import *
 
 agent = agents.BasicAgent(hidden_size, learning_rate)
 env = gym.make('Pong-v0')
 
+if print_analytics:
+	cProfile.run('main_function()') #Probably not very helpful
+else:
+	main_function()
 
+def main_function():
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+		for i in range(num_iterations):
+			print("Iteration %s. Resetting dataset" % i)
+			train_data = game.train_set()
 
-with tf.Session() as sess:
-	sess.run(tf.global_variables_initializer())
-	for i in range(num_iterations):
-		print("Iteration %s. Resetting dataset" % i)
-		train_data = game.train_set()
+			print("Starting self-play...")
+			for n in range(num_self_play_games):
+				if n%100 == 0:
+					print("Self-play game: %s" %n)
 
-		print("Starting self-play...")
-		for n in range(num_self_play_games):
-			if n%100 == 0:
-				print("Self-play game: %s" %n)
+				self_play(sess, agent, env, train_data)
 
-			self_play(sess, agent, env, train_data)
-
-		print("Starting training...")
-		for e in range(num_train_epochs):
-			diff_frames, actions, wins = train_data.sample(train_batch_size)
-			loss = agent.train(sess, diff_frames, actions, wins)
-			print("Loss epoch %s: %s" % (e, loss))
+			print("Starting training...")
+			for e in range(num_train_epochs):
+				diff_frames, actions, wins = train_data.sample(train_batch_size)
+				loss = agent.train(sess, diff_frames, actions, wins)
+				print("Loss epoch %s: %s" % (e, loss))
