d825b93428457535c84b1801089b2f082fbcdf32
Vladimir <vm@Vladimirs-laptop.local>
2018-02-03 18:44:22 +0000

    remove misplaced functions

--- game.py
+++ game.py
@@ -91,24 +91,6 @@ def self_play(session, agent, env, train_data):
     #env.render(close=True)
     return OpenAI_bot_score, agent_score
 
-#define preprocessing functions
-def preprocess(frame):
-	""" preprocess 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-	frame = frame[35:195] # crop
-	frame = frame[::2,::2,0] # downsample by factor of 2
-	frame[frame == 144] = 0 # erase background (background type 1)
-	frame[frame == 109] = 0 # erase background (background type 2)
-	frame[frame != 0] = 1 # everything else (paddles, ball) just set to 1
-	return frame.astype(np.float).ravel()
-
-def ex(preprocessed_frames):
-    '''expand array along first dimension, used if running the network with batch size = 1'''
-    return np.expand_dims(preprocessed_frames,0)
-
-def diff_frame(ordered_frames):
-	'''compares frames from the current and last timestep, in order to be able to capture motion'''
-	return preprocess(ordered_frames[0] - ordered_frames[1])
-
 agent = agents.BasicAgent(hidden_size, learning_rate)
 env = gym.make('Pong-v0')
 
