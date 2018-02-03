1ffe015a8c9f9bb079dfa4f66ebb72e127ae32a2
Vladimir <vm@Vladimirs-laptop.local>
2018-02-03 15:46:18 +0000

    pick a more reasonable hidden_size

--- testing.py
+++ testing.py
@@ -11,7 +11,7 @@ num_train_epochs = 100 #Change to something plausible later
 train_batch_size = 20
 num_iterations = 1
 learning_rate = 0.01
-hidden_size = 100000
+hidden_size = 100
 without_net = False
 quick_self_play = True #For testing
 
