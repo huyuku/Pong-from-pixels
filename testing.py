8b80b45dc700ad719eaca6f5780fa5b7a1b8dc10
Vladimir <vm@Vladimirs-laptop.local>
2018-02-03 15:07:28 +0000

    Fix more whitespace issues

--- testing.py
+++ testing.py
@@ -57,13 +57,13 @@ def self_play(session, agent, env, train_data):
 
 #define preprocessing functions
 def preprocess(frame):
-  """ preprocess 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-  frame = frame[35:195] # crop
-  frame = frame[::2,::2,0] # downsample by factor of 2
-  frame[frame == 144] = 0 # erase background (background type 1)
-  frame[frame == 109] = 0 # erase background (background type 2)
-  frame[frame != 0] = 1 # everything else (paddles, ball) just set to 1
-  return frame.astype(np.float).ravel()
+	""" preprocess 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+	frame = frame[35:195] # crop
+	frame = frame[::2,::2,0] # downsample by factor of 2
+	frame[frame == 144] = 0 # erase background (background type 1)
+	frame[frame == 109] = 0 # erase background (background type 2)
+	frame[frame != 0] = 1 # everything else (paddles, ball) just set to 1
+	return frame.astype(np.float).ravel()
 
 def diff_frame(ordered_frames):
 	return np.expand_dims(preprocess(ordered_frames[0] - ordered_frames[1]),0)
