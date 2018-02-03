16d4d01252259f17896baf143276f2fc5757273f
yordun <yordun@gmail.com>
2018-02-03 14:11:43 +0000

    Update agents.py to be up-to-date

--- agents.py
+++ agents.py
@@ -1,28 +1,33 @@
 import tensorflow as tf
+import numpy as np
 
 class BasicAgent():
-    '''This agent uses a 1-hidden-layer dense NN to compute probability of going UP'''
-    def __init__(self):
+    '''
+    This agent uses a 1-hidden-layer dense NN to compute probability of going UP.
+    '''
+    def __init__(self, learning_rate):
+        '''
+        learning_rate controls the learning rate of the gradient descent optimiser.
+        '''
 
         def weight_variable(shape):
-        	 initial = tf.truncated_normal(shape, stddev=0.05)
-        	 return tf.Variable(initial)
-          
+            initial = tf.truncated_normal(shape, stddev=0.05)
+            return tf.Variable(initial)
+
         self.W1 = weight_variable([80*80, hidden_size_1])
         self.W2 = weight_variable([hidden_size_1, 1])
+
         self.input_vectors = tf.placeholder(shape=(None, 80*80), dtype=tf.float32)
         self.actions       = tf.placeholder(shape=(None,1),      dtype=tf.float32)
         self.rewards       = tf.placeholder(shape=(None,1),      dtype=tf.float32)
-        
+
         self.hidden_layer = tf.nn.relu(tf.matmul(self.input_vectors, self.W1))
-        self.output_layer = tf.nn.sigmoid(tf.matmul(self.hidden_layer, self.W2))
-        
-        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.output_layer, labels=self.rewards))
-        
+        self.output_layer = tf.nn.sigmoid(tf.matmul(hidden_layer, self.W2))
+
+        self.loss = tf.reduce_sum(self.rewards * (self.actions * self.output_layer + (1-self.actions)*(1-self.output_layer)))
+
         self.GD = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-    
-    
-     
+        
     def action(self, sess, diff_frame):
         '''returns a probability of going UP at this frame'''
         feed_dict = {self.input_vectors:diff_frame}
