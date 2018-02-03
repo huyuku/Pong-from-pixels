a4821a24c2fdece3af9457154d735407fa4dd338
Vladimir <vm@Vladimirs-laptop.local>
2018-02-03 15:11:46 +0000

    agents.py: document the expected format of contents of the placeholder variables. document the loss function.
    testing.py:
    fix broken indentation. (not sure if it is correct, but was definitely
    broken before.)

--- agents.py
+++ agents.py
@@ -17,27 +17,28 @@ class BasicAgent():
         self.W1 = weight_variable([80*80, hidden_size_1])
         self.W2 = weight_variable([hidden_size_1, 1])
 
-        self.input_vectors = tf.placeholder(shape=(None, 80*80), dtype=tf.float32)
-        self.actions       = tf.placeholder(shape=(None,1),      dtype=tf.float32)
-        self.rewards       = tf.placeholder(shape=(None,1),      dtype=tf.float32)
+        self.input_vectors = tf.placeholder(shape=(None, 80*80), dtype=tf.float32)  # flattened diff_frame
+        self.actions       = tf.placeholder(shape=(None,1),      dtype=tf.float32)  # 1 if agent went UP, 0 otherwise
+        self.rewards       = tf.placeholder(shape=(None,1),      dtype=tf.float32)  # 1 if frame comes from a won game, -1 otherwise
 
         self.hidden_layer = tf.nn.relu(tf.matmul(self.input_vectors, self.W1))
         self.output_layer = tf.nn.sigmoid(tf.matmul(hidden_layer, self.W2))
 
+        # loss = - sum over i of reward_i * p(action_i | frame_i)
         self.loss = tf.reduce_sum(self.rewards * (self.actions * self.output_layer + (1-self.actions)*(1-self.output_layer)))
 
         self.GD = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-        
+
     def action(self, sess, diff_frame):
         '''returns a probability of going UP at this frame'''
         feed_dict = {self.input_vectors:diff_frame}
         predicted_action = sess.run(self.output_layer, feed_dict=feed_dict)[0,0]
         action = 3 + np.random.binomial(1, predicted_action)
         return action
-    
+
     def gym_action(self, sess, diff_frame):
         return 3 + self.action(self, sess, diff_frame)
-    	                                                           
+
     def train(self, sess, diff_frames, actions, wins):
         '''trains the agent on the data'''
         feed_dict={self.input_vector:diff_frames, self.actions:actions, self.rewards:wins}
--- testing.py
+++ testing.py
@@ -77,17 +77,17 @@ def mainfunction():
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		for i in range(num_iterations):
-		print("Iteration %s. Resetting dataset" % i)
-		train_data = train_set()
+			print("Iteration %s. Resetting dataset" % i)
+			train_data = train_set()
 
-		print("Starting self-play...")
-		for n in range(num_self_play_games):
-		  if n%100 == 0:
-			print("Self-play game: %s" %n)
+			print("Starting self-play...")
+			for n in range(num_self_play_games):
+			  if n%100 == 0:
+				print("Self-play game: %s" %n)
 
-		  self_play(sess, agent, env, train_data)
-		print("Starting training...")
-		for e in range(num_train_epochs):
-		  diff_frames, actions, wins = train_data.sample(train_batch_size)
-		  loss = agent.train(sess, diff_frames, actions, wins)
-		  print("Loss epoch %s: %s" % (e, loss))
+			  self_play(sess, agent, env, train_data)
+			print("Starting training...")
+			for e in range(num_train_epochs):
+			  diff_frames, actions, wins = train_data.sample(train_batch_size)
+			  loss = agent.train(sess, diff_frames, actions, wins)
+			  print("Loss epoch %s: %s" % (e, loss))
