d4f0e06f3dc5573e058fd31cad0a85596c2e6630
lagerros <jacobklagerros@gmail.com>
2018-02-03 17:38:21 +0000

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ num_iterations = 1
 learning_rate = 0.01
 hidden_size = 100
 without_net = False
-quick_self_play = True #For testing
+quick_self_play = bool(input("Quick self-play? 0 for no and 1 for yes") #For testing
 debug = False
 print_analytics = False
