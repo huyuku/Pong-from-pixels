c733b64a4e884d0917004338bc7410bfe774bce5
unknown <joar.mvs@gmail.com>
2018-02-04 13:46:32 +0000

    Logging added to main

--- logging_agent.py
+++ logging_agent.py
@@ -17,11 +17,25 @@ class Logging_Agent():
         return self.agent.gym_action(sess, diff_frame)
 
     def train(self, sess, diff_frames, actions, rewards):
-        self.timer.setstart()
+        self.logger.set_time_start()
         loss = self.agent.train(sess, diff_frames, actions, rewards)
-        self.timer.logtime('Training', 1)
+        self.logger.logtime('Training', 1)
         return loss
 
-    def log_matrices(sess):
+    def set_time_start(self):
+        self.logger.set_time_start():
+
+    def playing_log(self, number_of_games, wins, losses):
+        logger.loginfo("%s games, score: %s wins, %s losses" % (number_of_games, wins, losses))
+        logger.logtime('Playing iteration')
+        self.logger.set_time_start()
+
+    def epoch_log(self, sess, epoch_number, loss):
+        logger.loginfo("Epoch %s loss: %s" % (epoch_number, loss))
+        logger.logtime('Epoch iteration')
+        self.logger.set_time_start()
+        self.log_matrices(sess)
+
+    def log_matrices(self, sess):
         self.logger.log_matrix('W1', sess.run(self.agent.W1))
         self.logger.log_matrix('W2', sess.run(self.agent.W2))
--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import agents
 import game
 import data
 import logging
+import logging_agent
 from game import *
 from config import *
 
 agent = agents.BasicAgent(HIDDEN_SIZE, LEARNING_RATE)
+agent = logging_agent.Logging_Agent(agent)
 env = gym.make('Pong-v0')
 logging.basicConfig(filename='info.log',level=logging.INFO)
 logger = debugtools.Logger()
@@ -47,6 +49,9 @@ def main_function():
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         dataset = data.Dataset()
+        wins = 0
+        losses = 0
+        agent.set_time_start()
         for i in range(NUM_ITERATIONS):
             dataset.reset()
             print("Iteration {0}".format(i+1))
@@ -54,15 +59,21 @@ def main_function():
             for i_game in range(GAMES_PER_ITER):
                 f, a, r, o_s, a_s = game.create_play_data(sess, agent, env, without_net=WITHOUT_NET, quick_play=QUICK_PLAY)
                 dataset.add(f,a,r)
+                if a_s>o_s:
+                    wins += 1
+                else:
+                    losses += 1
                 if i_game % 20 == 0:
-                    print("Game {0}. Agent: {1}, Opponent: {2}".format(i_game, a_s, o_s))
+                    agent.playing_log(i_game, wins, losses)
+                    #print("Game {0}. Agent: {1}, Opponent: {2}".format(i_game, a_s, o_s))
             print("Size of dataset: {0}".format(dataset.size))
             print("Training:")
             for epoch in range(EPOCHS_PER_ITER):
                 f,a,r = dataset.sample(TRAIN_BATCH_SIZE)
                 loss = agent.train(sess,f,a,r)
                 if epoch % 500 == 0:
-                    print("Epoch {0}. Loss: {1}".format(epoch, loss))
+                    agent.epoch_log(sess, epoch, loss)
+                    #print("Epoch {0}. Loss: {1}".format(epoch, loss))
 
 
 if __name__ == "__main__":
